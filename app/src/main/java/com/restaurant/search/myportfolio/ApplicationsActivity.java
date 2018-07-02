b59a147df7468766d124f53e8787d6b0385958ca
shyamalpunekar <shyamal.punekar@gmail.com>
2018-07-02 12:33:18 -0700

    Add image

---
+++ app/src/main/java/com/restaurant/search/myportfolio/ApplicationsActivity.java
@@ -0,0 +1,13 @@
+package com.restaurant.search.myportfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ApplicationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_applications);
+    }
+}
--- app/src/main/java/com/restaurant/search/myportfolio/MainActivity.java
+++ app/src/main/java/com/restaurant/search/myportfolio/MainActivity.java
@@ -1,13 +1,27 @@
 package com.restaurant.search.myportfolio;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mFindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRepositoriesButton);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ApplicationsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
